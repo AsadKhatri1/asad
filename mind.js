db58d9f0be8453e56cc259bf664f8e0a0dd9ab8b
AsadKhatri1 <92842688+AsadKhatri1@users.noreply.github.com>
2023-09-02 13:28:41 +0500

    commiittedd

---
+++ mind.js
@@ -0,0 +1,16 @@
+const linkedin=()=>{
+    window.open(
+        "https://www.linkedin.com/in/asad-khatri-22ba971ba/", "_blank");
+}
+const facebook=()=>{
+    window.open(
+        "https://www.facebook.com/asad.khatri.96930", "_blank");
+}
+const github=()=>{
+    window.open(
+        "https://github.com/AsadKhatri1", "_blank");
+}
+
+const modal=()=>{
+    alert('My work will be uploaded soon')
+}
